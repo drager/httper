e52a26fc65f3a601a941b769a0995bc5e705e0cd
Jesper Håkansson <jesper@jesperh.se>
2018-08-05 00:20:32 +0200

    doc: Add get json example

---
+++ examples/get_json.rs
@@ -0,0 +1,29 @@
+extern crate httper;
+extern crate tokio;
+#[macro_use]
+extern crate serde_derive;
+
+use httper::client::{HttperClient, HttpsClient};
+use tokio::runtime::Runtime;
+
+fn main() {
+    let mut rt = Runtime::new().unwrap();
+
+    let httper_client = HttperClient::<HttpsClient>::new();
+
+    #[derive(Debug, Deserialize, PartialEq)]
+    struct Contributor {
+        id: u32,
+        login: String,
+    }
+
+    let result = rt.block_on(
+        httper_client
+            .get("https://api.github.com/repos/drager/httper/contributors")
+            .json::<Vec<Contributor>>(),
+    );
+
+    println!("Contributors: {:?}", result);
+
+    assert!(result.is_ok());
+}
