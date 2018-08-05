94df7996a7d77cdb221616d1686d8a9500873d4f
Jesper Håkansson <jesper@jesperh.se>
2018-08-05 23:26:05 +0200

    doc: Add example for post and chaining json

---
+++ examples/post_json.rs
@@ -0,0 +1,58 @@
+extern crate httper;
+extern crate hyper;
+extern crate tokio;
+#[macro_use]
+extern crate serde_derive;
+
+use httper::client::{HttperClient, HttpsClient};
+use hyper::rt::Future;
+use std::net::SocketAddr;
+use std::thread;
+use tokio::runtime::Runtime;
+
+/// Spins up a server using hyper.
+fn start_server(body: &'static [u8], addr: &SocketAddr) {
+    let new_svc = move || {
+        hyper::service::service_fn_ok(move |_req| hyper::Response::new(hyper::Body::from(body)))
+    };
+
+    let server = hyper::server::Server::bind(addr)
+        .serve(new_svc)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    hyper::rt::run(server);
+}
+
+fn main() {
+    let addr = ([127, 0, 0, 1], 9092).into();
+
+    let mut rt = Runtime::new().unwrap();
+
+    let buffer: &[u8] = br#"{"name": "Bumblebee"}"#;
+
+    // Spin up a temporary server so we can
+    // post some data somewhere.
+    thread::spawn(move || {
+        start_server(buffer, &addr);
+    });
+
+    let httper_client: HttperClient<HttpsClient> = HttperClient::<HttpsClient>::new();
+
+    #[derive(Debug, Deserialize)]
+    struct Transformer {
+        name: String,
+    }
+
+    let result = rt.block_on(
+        httper_client
+            .post(
+                &("http://".to_string() + &addr.to_string()),
+                hyper::Body::from(buffer),
+            )
+            .json::<Transformer>(),
+    );
+
+    println!("Result: {:?}", result);
+
+    assert!(result.is_ok());
+}
