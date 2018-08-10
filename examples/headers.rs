20d8ccd55218f8dfed482becfdbaef6a8246cafc
Jesper Håkansson <jesper@jesperh.se>
2018-08-10 13:16:35 +0200

    doc: Add headers example

---
+++ examples/headers.rs
@@ -0,0 +1,22 @@
+extern crate httper;
+extern crate tokio;
+
+use httper::client::HttperClient;
+use std::collections::HashMap;
+use tokio::runtime::Runtime;
+
+fn main() {
+    let mut rt = Runtime::new().unwrap();
+
+    let httper_client = HttperClient::new();
+
+    let mut headers = HashMap::new();
+    headers.insert("Content-Type".to_string(), "application/json".to_string());
+
+    // Call .send() to fire the request.
+    let result = rt.block_on(httper_client.get("https://www.rust-lang.org/en-US/").send());
+
+    println!("Result: {:?}", result);
+
+    assert!(result.is_ok());
+}
